0530a4441e4d49154d413a36d0500af844037ca0
Yu Kitazume <u.kitazume@gmail.com>
2016-11-25 17:19:31 -0800

    change docker utility instance name: docker -> docker1, docker2

--- cookbooks/docker_custom/attributes/default.rb
+++ cookbooks/docker_custom/attributes/default.rb
@@ -1,5 +1,5 @@
 default['docker_custom'] = {
-  :docker_instances => ["docker"]
+  :docker_instances => ["docker1", "docker2"]
 }
 
 default['docker_custom']['registries'] = %w(https://index.docker.io/v1/)
